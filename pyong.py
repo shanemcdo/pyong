93806b6c31c66d6c64392edcd4f15e8ee8830cc9
Shane McDonough <shanemcdman@gmail.com>
2023-10-05 11:14:16 -0700

    Set up basic drawing

--- pyong.py
+++ pyong.py
@@ -56,6 +56,32 @@ class PyongGame(pgt.GameScreen):
 		super().__init__(parent.real_screen, parent.real_window_size, parent.window_size, parent.frame_rate)
 		self.font = parent.font
 		self.player2_type = player2_type
+		self.paddle_size = Point(5, 50)
+		self.player1_paddle_pos = Point(3, 0)
+		self.player2_paddle_pos = Point(self.window_size.x - self.paddle_size.x - 3, 0)
+		ball_size = Point(6, 6)
+		self.ball_rect = pygame.Rect(*(self.window_size // 2 - ball_size // 2), *ball_size)
+		self.score = [0, 0]
+
+
+	def draw_paddle(self, position: Point):
+		pygame.draw.rect(self.screen, 'white', (position, self.paddle_size))
+
+	def draw_ball(self):
+		pygame.draw.rect(self.screen, 'white', self.ball_rect)
+
+	def draw_score(self):
+		p1_score = self.font.render(str(self.score[0]), False, 'white')
+		p2_score = self.font.render(str(self.score[1]), False, 'white')
+		self.screen.blit(p1_score, (self.window_size.x // 2 - p1_score.get_width() // 2 - 15, 3))
+		self.screen.blit(p2_score, (self.window_size.x // 2 - p2_score.get_width() // 2 + 15, 3))
+
+	def update(self):
+		self.screen.fill('black')
+		self.draw_paddle(self.player1_paddle_pos)
+		self.draw_paddle(self.player2_paddle_pos)
+		self.draw_ball()
+		self.draw_score()
 
 def main():
 	'''Driver Code'''
