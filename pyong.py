2d1e2d413e7c06141034931dc1378560796f317e
Shane McDonough <shanemcdman@gmail.com>
2023-10-05 10:54:00 -0700

    Implement CPU selection menu

--- pyong.py
+++ pyong.py
@@ -3,6 +3,12 @@
 import pygame_tools as pgt
 from pygame_tools import Point
 import pygame
+from enum import Enum
+
+class Player2Type(Enum):
+	HUMAN = 1
+	EASY_CPU = 2
+	HARD_CPU = 3
 
 class MainMenu(pgt.MenuScreen):
 	def __init__(self):
@@ -15,7 +21,7 @@ class MainMenu(pgt.MenuScreen):
 		self.title_font = pygame.font.Font(self.font_path, 50)
 		self.buttons = [
 			pgt.Button(CPUSelectMenu(self).run, '1 Player', (33, 150, 100, 50), self.font, border_size = 2, antialias = False),
-			pgt.Button(PyongGame(self).run, '2 Players', (167, 150, 100, 50), self.font, border_size = 2, antialias = False),
+			pgt.Button(PyongGame(self, Player2Type.HUMAN).run, '2 Players', (167, 150, 100, 50), self.font, border_size = 2, antialias = False),
 		]
 		self.title = self.title_font.render('PYONG', False, 'white')
 
@@ -25,14 +31,30 @@ class MainMenu(pgt.MenuScreen):
 
 class CPUSelectMenu(pgt.MenuScreen):
 	def __init__(self, parent: MainMenu):
-		super().__init__(parent.screen, parent.real_window_size, parent.window_size, parent.frame_rate)
+		print(parent.screen)
+		super().__init__(parent.real_screen, parent.real_window_size, parent.window_size, parent.frame_rate)
 		self.font = parent.font
-		self.game = PyongGame(self)
+		self.buttons = [
+			pgt.Button(PyongGame(self, Player2Type.EASY_CPU).run, 'Easy', (33, 150, 100, 50), self.font, border_size = 2, antialias = False),
+			pgt.Button(PyongGame(self, Player2Type.HARD_CPU).run, 'Hard', (167, 150, 100, 50), self.font, border_size = 2, antialias = False),
+			pgt.Button(self.stop, 'Back', (10, 10, 50, 30), self.font, border_size = 2, antialias = False)
+		]
+		self.title_font = pygame.font.Font(parent.font_path, 18)
+		self.title = self.title_font.render('CPU Difficulty', False, 'white')
+	
+	def stop(self):
+		self.running = False
+
+	def update(self):
+		super().update()
+		self.screen.blit(self.title, (self.window_size.x // 2 - self.title.get_width() / 2, 100))
 
 class PyongGame(pgt.GameScreen):
-	def __init__(self, parent: pgt.MenuScreen):
-		super().__init__(parent.screen, parent.real_window_size, parent.window_size, parent.frame_rate)
+	def __init__(self, parent: pgt.MenuScreen, player2_type: Player2Type):
+		super().__init__(parent.real_screen, parent.real_window_size, parent.window_size, parent.frame_rate)
 		self.font = parent.font
+		self.player2_type = player2_type
+		print(self.player2_type)
 
 def main():
 	'''Driver Code'''
